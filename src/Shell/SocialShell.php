83ae64905e677ed2a5379ff11d633c17afa89f3a
Wallef Antoine <antoine.wallef@gmail.com>
2018-01-30 12:38:33 +0100

    set options

--- src/Shell/SocialShell.php
+++ src/Shell/SocialShell.php
@@ -14,17 +14,27 @@ class SocialShell extends Shell
   public function getOptionParser()
   {
     $parser = parent::getOptionParser();
-    $parser->addSubcommand('facebook', [
+    $parser
+    ->addSubcommand('facebook', [
         'help' => 'Execute The Facebook Task.',
         'parser' => $this->Facebook->getOptionParser(),
-    ]);
-    $parser->addSubcommand('instagram', [
+    ])
+    ->addSubcommand('instagram', [
         'help' => 'Execute The Instagram Task.',
         'parser' => $this->Instagram->getOptionParser(),
-    ]);
-    $parser->addSubcommand('twitter', [
+    ])
+    ->addSubcommand('twitter', [
         'help' => 'Execute The Twitter Task.',
         'parser' => $this->Twitter->getOptionParser(),
+    ])
+    ->addOption('save', [
+        'short' => 's',
+        'help' => __('Save the post')
+    ])
+    ->addOption('model', [
+        'short' => 'm',
+        'help' => __('Specify a model to save the posts with...'),
+        'default' => 'Trois/Social.SocialPosts',
     ]);
     return $parser;
   }
